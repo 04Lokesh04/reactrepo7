9d2c2f6620fe6f22669dcacc3d33b8371bd74a84
04Lokesh04 <suresh.lokesh2003@gmail.com>
2024-04-09 10:34:58 +0000

    filters not working properly

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,76 @@
+import './index.css'
+import {FaSearch} from 'react-icons/fa'
+
+const FiltersGroup = props => {
+  const {
+    categoryOptions,
+    ratingsList,
+    changesearchitems,
+    changeCategory,
+    changeRating,
+    clearFilter,
+    details,
+  } = props
+  const {titleSearch} = details
+  const {categoryId} = categoryOptions
+  const {ratingId} = ratingsList
+  const categorychange = () => {
+    changeCategory(categoryId)
+  }
+  const ratingchange = () => {
+    changeRating(ratingId)
+  }
+  const clearall = () => {
+    clearFilter()
+  }
+  const changesearch = event => {
+    changesearchitems(event.target.value)
+  }
+  return (
+    <div className="filters-group-container">
+      <div className="searchcard">
+        <input
+          className="input"
+          type="search"
+          placeholder="search"
+          value={titleSearch}
+          onChange={changesearch}
+        />
+        <FaSearch className="sort-by-icon" />
+      </div>
+      <div className="categorycard">
+        <h1 className="headingis">Category</h1>
+        {categoryOptions.map(each => (
+          <button
+            key={each.categoryId}
+            className="categorybutton"
+            type="button"
+            onClick={categorychange}
+          >
+            {each.name}
+          </button>
+        ))}
+      </div>
+      <div className="ratingcard">
+        <h1 className="headingis">Rating</h1>
+        {ratingsList.map(each => (
+          <div className="rate">
+            <img
+              className="ratingimage"
+              src={each.imageUrl}
+              alt={`rating ${each.ratingId}`}
+              onClick={ratingchange}
+              key={each.ratingId}
+            />
+            <p className="rated">& up</p>
+          </div>
+        ))}
+      </div>
+      <button className="clearbutton" type="button" onClick={clearall}>
+        Clear Filters
+      </button>
+    </div>
+  )
+}
+
+export default FiltersGroup
